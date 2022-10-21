57bb094068dd12e74926e03144637384c9eb8c93
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-21 10:45:36 -0300

    feat: add ícones para edição e deleção de post

--- src/styles/PostStyles.js
+++ src/styles/PostStyles.js
@@ -1,76 +1,93 @@
 import styled from "styled-components";
+import { TiPencil } from "react-icons/ti";
+import { FaTrash } from "react-icons/fa";
 
 export default function PostStyles({ img, user, text }) {
-	return (
-		<>
-			<Container>
-				<Infos>
-					<img src={img} alt="" />
-				</Infos>
-				<Description>
-					<h1>{user}</h1>
-					<p>{text}</p>
-					<div></div>
-				</Description>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Infos>
+          <img src={img} alt="" />
+        </Infos>
+        <Description>
+          <span>
+            <h1>{user}</h1>
+            <h3>
+              <TiPencil />
+              <FaTrash />
+            </h3>
+          </span>
+          <p>{text}</p>
+          <div></div>
+        </Description>
+      </Container>
+    </>
+  );
 }
 
 const Container = styled.div`
-	margin-top: 30px;
-	padding: 18px;
-	width: 611px;
-	background-color: #171717;
-	height: 276px;
-	border-radius: 16px;
-	display: flex;
+  margin-top: 30px;
+  padding: 18px;
+  width: 611px;
+  background-color: #171717;
+  height: 276px;
+  border-radius: 16px;
+  display: flex;
 
-	@media screen and (max-width: 768px) {
-		width: 100%;
-		border-radius: 0;
-	}
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    border-radius: 0;
+  }
 `;
 
 const Infos = styled.div`
-	width: 50px;
-	height: 100%;
-	margin-right: 15px;
+  width: 50px;
+  height: 100%;
+  margin-right: 15px;
 
-	img {
-		width: 50px;
-		height: 50px;
-		border-radius: 50%;
-		object-fit: cover;
-	}
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
 `;
 
 const Description = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 
-	h1 {
-		font-family: "Lato", sans-serif;
-		font-size: 22px;
-		font-weight: 400;
-		color: #ffffff;
-		margin-bottom: 10px;
-	}
+  span {
+    font-family: "Lato", sans-serif;
+    font-size: 22px;
+    font-weight: 400;
+    color: #ffffff;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
 
-	p {
-		font-family: "Lato", sans-serif;
-		font-size: 17px;
-		font-weight: 400;
-		color: #b7b7b7;
-		margin-bottom: 10px;
-	}
+    h3 {
+      width: 50px;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 
-	div {
-		width: 100%;
-		height: 100%;
-		border: 1px solid #4d4d4d;
-		border-radius: 11px;
-	}
+  p {
+    font-family: "Lato", sans-serif;
+    font-size: 17px;
+    font-weight: 400;
+    color: #b7b7b7;
+    margin-bottom: 10px;
+  }
+
+  div {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #4d4d4d;
+    border-radius: 11px;
+  }
 `;
