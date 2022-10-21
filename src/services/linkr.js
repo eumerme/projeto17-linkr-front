88b48618f910785951212d2cd87079a0be71f766
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-21 13:45:19 -0300

    feat: criado função para atualizar texto do post

--- src/services/linkr.js
+++ src/services/linkr.js
@@ -3,56 +3,61 @@ import axios from "axios";
 const BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
 
 function createHeaders() {
-	const auth = JSON.parse(localStorage.getItem("linkr"));
+  const auth = JSON.parse(localStorage.getItem("linkr"));
 
-	if (auth !== null) {
-		console.log(auth.token);
-		const config = {
-			headers: {
-				Authorization: `Bearer ${auth.token}`,
-			},
-		};
-		return config;
-	}
+  if (auth !== null) {
+    console.log(auth.token);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${auth.token}`,
+      },
+    };
+    return config;
+  }
 }
 
 async function register(body) {
-	return axios.post(`${BASE_URL}/sign-up`, body);
+  return axios.post(`${BASE_URL}/sign-up`, body);
 }
 
 async function login(body) {
-	return axios.post(`${BASE_URL}/sign-in`, body);
+  return axios.post(`${BASE_URL}/sign-in`, body);
 }
 
 async function publish(body) {
-	const config = createHeaders();
-	return axios.post(`${BASE_URL}/timeline/publish`, body, config);
+  const config = createHeaders();
+  return axios.post(`${BASE_URL}/timeline/publish`, body, config);
 }
 
 async function listPosts() {
-	return axios.get(`${BASE_URL}/timeline/posts`);
+  return axios.get(`${BASE_URL}/timeline/posts`);
 }
 
 async function userLogout() {
-	const config = createHeaders();
-	return axios.post(`${BASE_URL}/logout`, {}, config);
+  const config = createHeaders();
+  return axios.post(`${BASE_URL}/logout`, {}, config);
 }
 
 async function listHashtags() {
-	return axios.get(`${BASE_URL}/hashtags`);
+  return axios.get(`${BASE_URL}/hashtags`);
 }
 
 async function listPostsbyHashtags(body) {
-	console.log(body);
-	return axios.get(`${BASE_URL}/hashtags/${body}`);
+  return axios.get(`${BASE_URL}/hashtags/${body}`);
+}
+
+async function editPostText(text, id) {
+  const config = createHeaders();
+  return axios.put(`${BASE_URL}/timeline/posts/update/${id}`, text, config);
 }
 
 export {
-	register,
-	login,
-	publish,
-	listPosts,
-	userLogout,
-	listHashtags,
-	listPostsbyHashtags,
+  register,
+  login,
+  publish,
+  listPosts,
+  userLogout,
+  listHashtags,
+  listPostsbyHashtags,
+  editPostText,
 };
