37cc19fdc11acbc05ab566b402eae6f27e48041f
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-26 17:12:55 -0300

    feat: carregamento de posts pelo scroll infinito

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,58 +1,79 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useState } from "react";
 import { listPosts } from "../../services/linkr";
 import TimelineMainLayout from "./TimelineMainLayout";
 import PostsMainLayout from "../Posts/PostsMainLayout";
 import styled from "styled-components";
 import Loading from "../commom/Loading";
-import UploadContext from "../../Contexts/UploadContext";
 import PublishBox from "./PublishBox";
+import InfiniteScroll from "react-infinite-scroller";
 
 function Timeline() {
   const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [needRender, setNeedRender] = useState(true);
   const [existPost, setExistPost] = useState(null);
   const [errorServer, setErrorServer] = useState(false);
   const [empty, setEmpty] = useState(false);
-  const { upload } = useContext(UploadContext);
 
   useEffect(() => {
-    setTimeout(function () {
-      listPosts()
-        .then((data) => {
-          if (data.data.followSomeone === true) {
-            setPosts(Array.from(data.data.posts));
-            if (data.data.posts.length === 0) setEmpty(true);
-            else setExistPost(true);
-          } else {
-            if (data.data.posts.length === 0) setExistPost(false);
-            else setPosts(Array.from(data.data.posts));
-          }
-        })
-        .catch((error) => {
-          setErrorServer(true);
-        });
-    }, 500);
-  }, [upload]);
+    listPosts()
+      .then((data) => {
+        if (data.data.followSomeone === true) {
+          setPosts(Array.from(data.data.posts));
+          if (data.data.posts.length === 0) setEmpty(true);
+          else setExistPost(true);
+        } else {
+          if (data.data.posts.length === 0) setExistPost(false);
+          else setPosts(Array.from(data.data.posts));
+        }
+      })
+      .catch((error) => {
+        setErrorServer(true);
+      });
+  }, []);
 
+  function loaderPosts() {
+    console.log("chamou");
+    setNeedRender(false);
+    if (posts.length === 0) setNeedRender(true);
+    setTimeout(() => {
+      const partOfPosts = posts.slice(allPosts.length, allPosts.length + 10);
+      setAllPosts(allPosts.concat(partOfPosts));
+
+      if (posts.length > allPosts.length) {
+        setNeedRender(true);
+      }
+    }, 2000);
+  }
   return (
     <TimelineMainLayout timeline={true}>
       <Homescreen>
         <Title>timeline</Title>
         <PublishBox />
-        {posts.length !== 0 ? (
-          posts.map((value, index) => (
-            <PostsMainLayout
-              key={index}
-              id={value.id}
-              img={value.imageUrl}
-              url={value.url}
-              text={value.text}
-              userId={value.userId}
-              name={value.name}
-            />
-          ))
-        ) : (
-          <Loading error={errorServer} empty={empty} existPost={existPost} />
-        )}
+        <InfiniteScroll
+          pageStart={1}
+          loadMore={loaderPosts}
+          hasMore={needRender}
+          loader={
+            <Loading error={errorServer} empty={empty} existPost={existPost} />
+          }
+        >
+          {allPosts.length > 0 ? (
+            allPosts.map((value, index) => (
+              <PostsMainLayout
+                key={index}
+                id={value.id}
+                img={value.imageUrl}
+                url={value.url}
+                text={value.text}
+                userId={value.userId}
+                name={value.name}
+              />
+            ))
+          ) : (
+            <Loading />
+          )}
+        </InfiniteScroll>
       </Homescreen>
     </TimelineMainLayout>
   );
