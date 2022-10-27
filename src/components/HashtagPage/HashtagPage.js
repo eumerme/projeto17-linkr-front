fb782eee36de876d71eca2cb674fac94451e9c0c
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-27 10:06:36 -0300

    fix: re-render infinito dos posts

--- src/components/HashtagPage/HashtagPage.js
+++ src/components/HashtagPage/HashtagPage.js
@@ -8,54 +8,56 @@ import Loading from "../commom/Loading";
 import InfiniteScroll from "react-infinite-scroller";
 
 export default function HashtagPage() {
-	const params = useParams();
-	const [posts, setPosts] = useState([]);
-	const [needRender, setNeedRender] = useState(true);
-	const [errorServer, setErrorServer] = useState(false);
-	const [empty, setEmpty] = useState(false);
+  const params = useParams();
+  const [posts, setPosts] = useState([]);
+  const [needRender, setNeedRender] = useState(true);
+  const [errorServer, setErrorServer] = useState(false);
+  const [empty, setEmpty] = useState(false);
 
-	function loaderPosts() {
-		setNeedRender(false);
-		listPostsbyHashtags(params.hashtag, posts.length + 10)
-			.then((data) => {
-				setPosts(data.data);
-				if (data.data.length === 0) setEmpty(true);
-				setNeedRender(true);
-			})
-			.catch((error) => {
-				setErrorServer(true);
-			});
-	}
+  function loaderPosts() {
+    setNeedRender(false);
+    listPostsbyHashtags(params.hashtag, posts.length + 10)
+      .then((data) => {
+        setPosts(data.data);
+        if (data.data.length === 0) setEmpty(true);
+        if (posts.length < data.data.length) {
+          setNeedRender(true);
+        }
+      })
+      .catch((error) => {
+        setErrorServer(true);
+      });
+  }
 
-	return (
-		<>
-			<TimelineMainLayout hashtag={true}>
-				<Homescreen>
-					<Title id="title"># {params.hashtag}</Title>
-					<InfiniteScroll
-						pageStart={1}
-						loadMore={loaderPosts}
-						hasMore={needRender}
-						loader={<Loading error={errorServer} empty={empty} />}
-					>
-						{posts.length > 0 ? (
-							posts.map((value, index) => (
-								<PostsMainLayout
-									key={index}
-									id={value.id}
-									img={value.imageUrl}
-									url={value.url}
-									text={value.text}
-									userId={value.userId}
-									name={value.name}
-								/>
-							))
-						) : (
-							<Loading error={errorServer} empty={empty} />
-						)}
-					</InfiniteScroll>
-				</Homescreen>
-			</TimelineMainLayout>
-		</>
-	);
+  return (
+    <>
+      <TimelineMainLayout hashtag={true}>
+        <Homescreen>
+          <Title id="title"># {params.hashtag}</Title>
+          <InfiniteScroll
+            pageStart={1}
+            loadMore={loaderPosts}
+            hasMore={needRender}
+            loader={<Loading error={errorServer} empty={empty} />}
+          >
+            {posts.length > 0 ? (
+              posts.map((value, index) => (
+                <PostsMainLayout
+                  key={index}
+                  id={value.id}
+                  img={value.imageUrl}
+                  url={value.url}
+                  text={value.text}
+                  userId={value.userId}
+                  name={value.name}
+                />
+              ))
+            ) : (
+              <Loading error={errorServer} empty={empty} />
+            )}
+          </InfiniteScroll>
+        </Homescreen>
+      </TimelineMainLayout>
+    </>
+  );
 }
